82c08f5cac618769b1d472297959c4fd3054959b
simonmitchell <simon.mitchell@digital.justice.gov.uk>
2023-04-19 18:31:01 +0100

    HAAR-1033: ⬆️ update springboot 3

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "4.8.6-beta-2"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "5.1.3"
   kotlin("plugin.spring") version "1.8.10"
 }
 
@@ -7,6 +7,10 @@ configurations {
   testImplementation { exclude(group = "org.junit.vintage") }
 }
 
+repositories {
+  maven { url = uri("https://repo.spring.io/milestone") }
+  mavenCentral()
+}
 dependencies {
   implementation("org.springframework.boot:spring-boot-starter-webflux")
 }
