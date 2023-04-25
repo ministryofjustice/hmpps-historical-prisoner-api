6e562635d179cd520141a475db6c621159415580
SimonMitchellMOJ <51327941+SimonMitchellMOJ@users.noreply.github.com>
2023-04-25 16:10:51 +0100

    HAAR-1033: ⬆️ update spring boot 3 (#31)
    * HAAR-1033: ⬆️ update springboot 3
    
    * HAAR-1033: ⬆️ update springboot 3
    
    * update spring boot plugin version
    
    * update kotlin version

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,16 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "4.8.6-beta-2"
-  kotlin("plugin.spring") version "1.8.10"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "5.1.4-beta-3"
+  kotlin("plugin.spring") version "1.8.20"
 }
 
 configurations {
   testImplementation { exclude(group = "org.junit.vintage") }
 }
 
+repositories {
+  maven { url = uri("https://repo.spring.io/milestone") }
+  mavenCentral()
+}
 dependencies {
   implementation("org.springframework.boot:spring-boot-starter-webflux")
 }
