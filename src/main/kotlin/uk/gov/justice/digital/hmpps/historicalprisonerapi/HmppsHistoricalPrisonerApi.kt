0697acb9599b83515e8ea8111f0fa64ed2e2051e
petergphillips <5099053+petergphillips@users.noreply.github.com>
2024-11-06 15:54:12 +0000

    SDIT-2186: ✨ Initial rename project commit (#1)

--- src/main/kotlin/uk/gov/justice/digital/hmpps/historicalprisonerapi/HmppsHistoricalPrisonerApi.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/historicalprisonerapi/HmppsHistoricalPrisonerApi.kt
@@ -1,11 +1,11 @@
-package uk.gov.justice.digital.hmpps.templatepackagename
+package uk.gov.justice.digital.hmpps.historicalprisonerapi
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class HmppsTemplateKotlin
+class HmppsHistoricalPrisonerApi
 
 fun main(args: Array<String>) {
-  runApplication<HmppsTemplateKotlin>(*args)
+  runApplication<HmppsHistoricalPrisonerApi>(*args)
 }
