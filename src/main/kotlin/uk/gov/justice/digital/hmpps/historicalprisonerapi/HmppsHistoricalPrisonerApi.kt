92647e2a44c94e9c9160bccb8c67c587ab098729
Peter Phillips <5099053+petergphillips@users.noreply.github.com>
2024-11-13 16:55:37 +0000

    SDIT-2237: ✨ Add in identifier searching

--- src/main/kotlin/uk/gov/justice/digital/hmpps/historicalprisonerapi/HmppsHistoricalPrisonerApi.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/historicalprisonerapi/HmppsHistoricalPrisonerApi.kt
@@ -2,8 +2,11 @@ package uk.gov.justice.digital.hmpps.historicalprisonerapi
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.data.web.config.EnableSpringDataWebSupport
+import org.springframework.data.web.config.EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO
 
 @SpringBootApplication
+@EnableSpringDataWebSupport(pageSerializationMode = VIA_DTO)
 class HmppsHistoricalPrisonerApi
 
 fun main(args: Array<String>) {
