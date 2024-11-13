5f274be93843c1bce8787df9db8c0f6552ef551e
Peter Phillips <5099053+petergphillips@users.noreply.github.com>
2024-11-13 15:11:15 +0000

    SDIT-2237: ✨ Add in logic for address lookup

---
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/historicalprisonerapi/service/AddressLookupServiceTest.kt
@@ -0,0 +1,40 @@
+package uk.gov.justice.digital.hmpps.historicalprisonerapi.service
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import org.mockito.Mockito.anyString
+import org.mockito.Mockito.mock
+import org.mockito.Mockito.verify
+import org.mockito.kotlin.whenever
+import uk.gov.justice.digital.hmpps.historicalprisonerapi.repository.AddressLookupRepository
+
+class AddressLookupServiceTest {
+  private val addressLookupRepository: AddressLookupRepository = mock()
+  private val addressLookupService = AddressLookupService(addressLookupRepository)
+
+  @Test
+  fun `should find prisoners with addresses`() {
+    val addressTerms = "John Smith"
+    val expectedPrisoners = listOf("AB111111", "AB111112")
+
+    whenever(addressLookupRepository.findPrisonersWithAddresses(anyString())).thenReturn(expectedPrisoners)
+
+    val foundPrisoners = addressLookupService.findPrisonersWithAddresses(addressTerms)
+    assertThat(foundPrisoners).containsExactlyInAnyOrderElementsOf(expectedPrisoners)
+
+    verify(addressLookupRepository).findPrisonersWithAddresses("NEAR((John, Smith), 5, TRUE)")
+  }
+
+  @Test
+  fun `should remove unnecessary characters before calling repository`() {
+    val addressTerms = "   John  Smith and .,  John's friend "
+    val expectedPrisoners = listOf("AB111111", "AB111112")
+
+    whenever(addressLookupRepository.findPrisonersWithAddresses(anyString())).thenReturn(expectedPrisoners)
+
+    val foundPrisoners = addressLookupService.findPrisonersWithAddresses(addressTerms)
+    assertThat(foundPrisoners).containsExactlyInAnyOrderElementsOf(expectedPrisoners)
+
+    verify(addressLookupRepository).findPrisonersWithAddresses("NEAR((John, Smith, and, Johns, friend), 5, TRUE)")
+  }
+}
