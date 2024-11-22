9d039014970baf633dc32fbf3b7830422344d3a7
petergphillips <5099053+petergphillips@users.noreply.github.com>
2024-11-22 15:40:47 +0000

    SDIT-2237: ✨ Add in address lookup term sanitisation (#13)

--- src/main/kotlin/uk/gov/justice/digital/hmpps/historicalprisonerapi/service/AddressLookupService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/historicalprisonerapi/service/AddressLookupService.kt
@@ -5,6 +5,15 @@ import uk.gov.justice.digital.hmpps.historicalprisonerapi.repository.AddressLook
 
 @Service
 class AddressLookupService(private val addressLookupRepository: AddressLookupRepository) {
-  fun findPrisonersWithAddresses(addressTerms: String): List<String> =
-    addressLookupRepository.findPrisonersWithAddresses("NEAR(($addressTerms), 5, TRUE)")
+  fun findPrisonersWithAddresses(addressTerms: String): List<String> {
+    // duplicate logic from HPA front end
+    val searchTerm = addressTerms
+      .replace(".", " ")
+      .replace(",", " ")
+      .replace("'", "")
+      .trim()
+      .split("\\s+".toRegex())
+      .joinToString(", ") { it.trim() }
+    return addressLookupRepository.findPrisonersWithAddresses("NEAR(($searchTerm), 5, TRUE)")
+  }
 }
