644e7c91af2b6a31ebeb8b790109f4ce1d841bb6
petergphillips <5099053+petergphillips@users.noreply.github.com>
2024-11-26 08:47:13 +0000

    SDIT-2237: 🎨 Only map needed fields and ignore others (#17)

--- src/main/kotlin/uk/gov/justice/digital/hmpps/historicalprisonerapi/model/Prisoner.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/historicalprisonerapi/model/Prisoner.kt
@@ -1,5 +1,6 @@
 package uk.gov.justice.digital.hmpps.historicalprisonerapi.model
 
+import com.fasterxml.jackson.annotation.JsonIgnore
 import jakarta.persistence.Column
 import jakarta.persistence.Entity
 import jakarta.persistence.GeneratedValue
@@ -13,77 +14,82 @@ import java.time.LocalDate
 
 @Entity
 @Table(name = "PRISONERS", schema = "HPA")
-open class Prisoner {
+data class Prisoner(
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   @Column(name = "PK_PRISONER", nullable = false)
-  open var id: Int? = null
+  @JsonIgnore
+  val id: Int? = null,
 
   @Size(max = 8)
   @NotNull
   @Column(name = "PRISON_NUMBER", nullable = false, length = 8)
-  open var prisonNumber: String? = null
+  val prisonNumber: String? = null,
 
   @NotNull
   @Column(name = "PERSON_IDENTIFIER", nullable = false, precision = 10)
-  open var personIdentifier: BigDecimal? = null
+  @JsonIgnore
+  val personIdentifier: BigDecimal? = null,
 
   @Size(max = 24)
   @Column(name = "SURNAME", length = 24)
-  open var surname: String? = null
+  val surname: String? = null,
 
   @Size(max = 13)
   @Column(name = "FORENAME_1", length = 13)
-  open var forename1: String? = null
+  val forename1: String? = null,
 
   @Size(max = 13)
   @Column(name = "FORENAME_2", length = 13)
-  open var forename2: String? = null
+  val forename2: String? = null,
 
   @Column(name = "BIRTH_DATE")
-  open var birthDate: LocalDate? = null
+  @JsonIgnore
+  val birthDate: LocalDate? = null,
 
   @NotNull
   @Column(name = "IS_ALIAS", nullable = false)
-  open var isAlias: Boolean? = false
+  val isAlias: Boolean? = false,
 
   @Column(name = "SEX")
-  open var sex: Char? = null
+  @JsonIgnore
+  val sex: Char? = null,
 
   @Size(max = 14)
   @Column(name = "PNC_NUMBER", length = 14)
-  open var pncNumber: String? = null
+  @JsonIgnore
+  val pncNumber: String? = null,
 
   @Size(max = 14)
   @Column(name = "CRO_NUMBER", length = 14)
-  open var croNumber: String? = null
+  @JsonIgnore
+  val croNumber: String? = null,
 
   @NotNull
   @Column(name = "HAS_HDC", nullable = false)
-  open var hasHdc: Boolean? = false
+  @JsonIgnore
+  val hasHdc: Boolean? = false,
 
   @NotNull
   @Column(name = "IS_LIFER", nullable = false)
-  open var isLifer: Boolean? = false
+  @JsonIgnore
+  val isLifer: Boolean? = false,
 
   @Column(name = "RECEPTION_DATE")
-  open var receptionDate: LocalDate? = null
+  val receptionDate: LocalDate? = null,
 
   @Size(max = 24)
   @Column(name = "PRIMARY_SURNAME", length = 24)
-  open var primarySurname: String? = null
+  val primarySurname: String? = null,
 
   @Size(max = 13)
   @Column(name = "PRIMARY_FORENAME_1", length = 13)
-  open var primaryForename1: String? = null
-
-  @Column(name = "PRIMARY_INITIAL")
-  open var primaryInitial: Char? = null
+  val primaryForename1: String? = null,
 
   @Size(max = 13)
   @Column(name = "PRIMARY_FORENAME_2", length = 13)
-  open var primaryForename2: String? = null
+  val primaryForename2: String? = null,
 
   @Column(name = "PRIMARY_BIRTH_DATE")
-  open var primaryBirthDate: LocalDate? = null
-}
+  val primaryBirthDate: LocalDate? = null,
+)
