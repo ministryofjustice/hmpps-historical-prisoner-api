87d0411d6e36faf1daa218d11931ad0fe0d352e8
Peter Phillips <5099053+petergphillips@users.noreply.github.com>
2024-12-02 11:41:20 +0000

    SDIT-2237: ✨ Add gender etc. to other searches too

--- src/test/kotlin/uk/gov/justice/digital/hmpps/historicalprisonerapi/service/AddressLookupServiceTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/historicalprisonerapi/service/AddressLookupServiceTest.kt
@@ -2,6 +2,7 @@ package uk.gov.justice.digital.hmpps.historicalprisonerapi.service
 
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
+import org.mockito.ArgumentMatchers.anyBoolean
 import org.mockito.ArgumentMatchers.anyString
 import org.mockito.Mockito.mock
 import org.mockito.Mockito.verify
@@ -23,35 +24,71 @@ class AddressLookupServiceTest {
   fun `should find prisoners with addresses`() {
     val addressTerms = "John Smith"
 
-    whenever(prisonerRepository.findByAddresses(anyString(), any())).thenReturn(expectedPrisoners)
+    whenever(prisonerRepository.findByAddresses(anyString(), anyString(), anyBoolean(), anyBoolean(), any())).thenReturn(expectedPrisoners)
 
-    val foundPrisoners = addressLookupService.findPrisonersWithAddresses(addressTerms, pageRequest)
+    val foundPrisoners = addressLookupService.findPrisonersWithAddresses(
+      addressTerms = addressTerms,
+      gender = "Y",
+      hdc = false,
+      lifer = true,
+      pageRequest = pageRequest,
+    )
     assertThat(foundPrisoners).containsExactlyInAnyOrderElementsOf(expectedPrisoners)
 
-    verify(prisonerRepository).findByAddresses("NEAR((John, Smith), 5, TRUE)", pageRequest)
+    verify(prisonerRepository).findByAddresses(
+      addressTerms = "NEAR((John, Smith), 5, TRUE)",
+      gender = "Y",
+      hdc = false,
+      lifer = true,
+      pageRequest = pageRequest,
+    )
   }
 
   @Test
   fun `should remove unnecessary characters before calling repository`() {
     val addressTerms = "John  Smith and .,  John's friend"
 
-    whenever(prisonerRepository.findByAddresses(anyString(), any())).thenReturn(expectedPrisoners)
+    whenever(prisonerRepository.findByAddresses(anyString(), anyString(), anyBoolean(), anyBoolean(), any())).thenReturn(expectedPrisoners)
 
-    val foundPrisoners = addressLookupService.findPrisonersWithAddresses(addressTerms, pageRequest)
+    val foundPrisoners = addressLookupService.findPrisonersWithAddresses(
+      addressTerms = addressTerms,
+      gender = "Y",
+      hdc = false,
+      lifer = true,
+      pageRequest = pageRequest,
+    )
     assertThat(foundPrisoners).containsExactlyInAnyOrderElementsOf(expectedPrisoners)
 
-    verify(prisonerRepository).findByAddresses("NEAR((John, Smith, and, Johns, friend), 5, TRUE)", pageRequest)
+    verify(prisonerRepository).findByAddresses(
+      addressTerms = "NEAR((John, Smith, and, Johns, friend), 5, TRUE)",
+      gender = "Y",
+      hdc = false,
+      lifer = true,
+      pageRequest = pageRequest,
+    )
   }
 
   @Test
   fun `should trim at start and end as well before calling repository`() {
     val addressTerms = "  John  Smith and John's friend   "
 
-    whenever(prisonerRepository.findByAddresses(anyString(), any())).thenReturn(expectedPrisoners)
+    whenever(prisonerRepository.findByAddresses(anyString(), anyString(), anyBoolean(), anyBoolean(), any())).thenReturn(expectedPrisoners)
 
-    val foundPrisoners = addressLookupService.findPrisonersWithAddresses(addressTerms, pageRequest)
+    val foundPrisoners = addressLookupService.findPrisonersWithAddresses(
+      addressTerms = addressTerms,
+      gender = "Y",
+      hdc = false,
+      lifer = true,
+      pageRequest = pageRequest,
+    )
     assertThat(foundPrisoners).containsExactlyInAnyOrderElementsOf(expectedPrisoners)
 
-    verify(prisonerRepository).findByAddresses("NEAR((John, Smith, and, Johns, friend), 5, TRUE)", pageRequest)
+    verify(prisonerRepository).findByAddresses(
+      addressTerms = "NEAR((John, Smith, and, Johns, friend), 5, TRUE)",
+      gender = "Y",
+      hdc = false,
+      lifer = true,
+      pageRequest = pageRequest,
+    )
   }
 }
