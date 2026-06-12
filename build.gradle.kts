e5a89dbddae23d0e923ed613b08636118cb57234
Peter Phillips <5099053+petergphillips@users.noreply.github.com>
2026-06-12 13:49:44 +0100

    SDIT-3887: ⬆️ Upgrade to latest libs and tidy

--- build.gradle.kts
+++ build.gradle.kts
@@ -30,16 +30,13 @@ dependencies {
   testImplementation("org.springframework.boot:spring-boot-starter-webclient-test")
   testImplementation("org.springframework.boot:spring-boot-starter-webflux-test")
   testImplementation("org.wiremock:wiremock-standalone:3.13.2")
-  testImplementation("io.swagger.parser.v3:swagger-parser:2.1.37") {
+  testImplementation("io.swagger.parser.v3:swagger-parser:2.1.42") {
     exclude(group = "io.swagger.core.v3")
   }
 }
 
 kotlin {
   jvmToolchain(25)
-  compilerOptions {
-    freeCompilerArgs.addAll("-Xannotation-default-target=param-property")
-  }
 }
 
 tasks {
